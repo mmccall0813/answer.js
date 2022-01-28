86d5c7009af78053236f2d7c244508c39841b81b
mmccall0813 <masonjamesmccall0813@gmail.com>
2022-01-28 09:58:11 -0500

    forgot to make the readme bookmarklet an actual bookmarklet

--- make.js
+++ make.js
@@ -4,4 +4,4 @@ var minified = minfiy(fs.readFileSync("./answer.js")).code;
 
 fs.writeFileSync("minified.js", minified)
 fs.writeFileSync("loader.html", `<a href="javascript:${encodeURIComponent(minified)}">answer.js</a>`)
-fs.writeFileSync("README.md", `${fs.readFileSync("./readme-template.md")}\n\`\`\`\n${minified}\n\`\`\``)
+fs.writeFileSync("README.md", `${fs.readFileSync("./readme-template.md")}\n\`\`\`\njavascript:${encodeURIComponent(minified)}\n\`\`\``)
