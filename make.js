da79966b4b0175bc515489e99f0ec879c9d57634
mmccall0813 <masonjamesmccall0813@gmail.com>
2022-01-28 09:45:17 -0500

    forgot a newline

--- make.js
+++ make.js
@@ -4,4 +4,4 @@ var minified = minfiy(fs.readFileSync("./answer.js")).code;
 
 fs.writeFileSync("minified.js", minified)
 fs.writeFileSync("loader.html", `<a href="javascript:${encodeURIComponent(minified)}">answer.js</a>`)
-fs.writeFileSync("README.md", `${fs.readFileSync("./readme-template.md")}\n\`\`\`\n${minified}\`\`\``)
+fs.writeFileSync("README.md", `${fs.readFileSync("./readme-template.md")}\n\`\`\`\n${minified}\n\`\`\``)
