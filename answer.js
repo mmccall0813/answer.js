2567adc56efbeab8573a006a3ee6dd825a1c5dc0
mmccall0813 <masonjamesmccall0813@gmail.com>
2022-03-15 11:48:05 -0400

    Update class names should be unpatched, havent tested yet

--- answer.js
+++ answer.js
@@ -1,14 +1,14 @@
 (() => {
-var idinput = document.querySelector(".styles__idInput___3PMeH-camelCase");
+var idinput = document.querySelector(".styles__idInput___1zWUq-camelCase");
 if(!idinput){
     return alert("Please only run this script on the start screen (where you input id)");
 }
-var form = document.querySelector(".styles__mainBox___2YQWX-camelCase");
+var form = document.querySelector(".styles__mainBox___16RUM-camelCase");
 form.onsubmit = function(...args){
     alert(`Starting bot with game code ${idinput.value}. If you incorrectly entered the game code, please refresh the page.`);
     start(idinput.value);
 }
-var joinButton = document.querySelector(".styles__joinButton___1wofq-camelCase");
+var joinButton = document.querySelector(".styles__joinButton___xSaJN-camelCase");
 joinButton.onclick = function(...args){
     form.onsubmit()
 }
@@ -42,18 +42,18 @@ async function start(gameid){
         })
     })
     setInterval( () => {
-        var questionText = document.querySelector(".styles__questionText___10zyP-camelCase")
-        var feedback = document.querySelector(".styles__feedbackContainer___2UC04-camelCase > div");
+        var questionText = document.querySelector(".styles__questionText___2MlSZ-camelCase")
+        var feedback = document.querySelector(".styles__feedbackContainer___1fuws-camelCase > div");
         if(feedback) feedback.click();
         switch(mode){ // mode-specific stuff
             case "Gold": // auto-click a chest
             var chests = document.querySelectorAll("div[class^=\"styles__choice\"");
             if(chests.length == 3) chests[Math.floor(Math.random()*3)].click();
-            var header = document.querySelector(".styles__headerInside___1fSu9-camelCase");
-            if(header && header.innerText) document.querySelector(".arts__regularBody___1st6G-camelCase").click()
-            var noPlayersNext = document.querySelector(".styles__noPlayers___3J6Gz-camelCase > div");
+            var header = document.querySelector(".styles__headerInside___x--63-camelCase");
+            if(header && header.innerText) document.querySelector(".arts__regularBody___1TM6E-camelCase").click()
+            var noPlayersNext = document.querySelector(".styles__noPlayers___1Wz34-camelCase > div");
             if(noPlayersNext) noPlayersNext.click();
-            var firstPlayerToSteal = document.querySelector(".styles__playerContainer___2pb9Q-camelCase");
+            var firstPlayerToSteal = document.querySelector(".styles__playerContainer___3zoyU-camelCase");
             if(firstPlayerToSteal) firstPlayerToSteal.click(); // might swap with lower player if its a swap
             break;
         }
@@ -62,7 +62,7 @@ async function start(gameid){
         
         var answered = false;
         for(var i = 0; i < 4 && answered == false; i++){
-            var button = document.querySelectorAll(".styles__answerContainer___qj_oZ-camelCase")[i];
+            var button = document.querySelectorAll(".styles__answerContainer___3WS-k-camelCase")[i];
             if(button.innerText && question.correctAnswers.includes(button.innerText)){
                 button.click();
                 answered = true;
